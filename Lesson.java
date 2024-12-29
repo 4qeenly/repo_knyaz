2e1c9b33998ceba17bf924335400c3fd582b0e0a
knyaz <knyaz20051710@gmail.com>
2024-12-30 00:49:43 +0400

    cikl for

--- Lesson.java
+++ Lesson.java
@@ -1,6 +1,11 @@
 public class Lesson {
     public static void main(String[] args) {
-        
+        System.out.println("Hello mir");
+        for (int i = 0; i < 9 ; i++) {
+            if(i % 2 ==1 ){
+                System.out.println(i);
+            }
+        }
 
     }
 }
